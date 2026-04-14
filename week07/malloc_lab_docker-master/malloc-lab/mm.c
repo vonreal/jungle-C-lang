6fe51e5efc905e4a974a5baec36f149ad5244858
jiun <vonreal20@gmail.com>
2026-04-14 19:10:39 +0900

    feat(malloc-lab): 1차 구현(realloc제외)

--- week07/malloc_lab_docker-master/malloc-lab/mm.c
+++ week07/malloc_lab_docker-master/malloc-lab/mm.c
@@ -24,9 +24,9 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "Naco",
+    "VoN",
     /* First member's full name */
-    "nacotti",
+    "naco",
     /* First member's email address */
     "vonreal20@gmail.com",
     /* Second member's full name (leave blank if none) */
@@ -42,7 +42,7 @@ team_t team = {
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
 
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t))) // size_t = 현재 8바이트임. 8바이트를 8의배수로 정렬하겠다! (헤더+푸터 사이즈)
 
 #define CHUNKSIZE (1 << 12)
 
@@ -58,8 +58,8 @@ team_t team = {
 #define NEXT_BLOCK_PTR(bp) ((char *)(bp) + (GET_SIZE(HDRP(bp))))
 #define PREV_BLOCK_PTR(bp) ((char *)(bp) - (GET_SIZE(FDRP(bp))))
 
-#define GET_SIZE(p) *(unsigned int *)(p) & ~0x07
-#define GET_ALLOC(p) *(unsigned int *)(p) & 0x01
+#define GET_SIZE(p) (*(unsigned int *)(p) & ~0x07)
+#define GET_ALLOC(p) (*(unsigned int *)(p) & 0x01)
 
 char *heap_listp;
 
@@ -69,10 +69,8 @@ char *heap_listp;
 int mm_init(void)
 {
     // 1. 4 word 초기화하기
-    printf("before heap: %p\n", mem_heap_hi());
     if ((heap_listp = mem_sbrk(4 * WORD_SIZE)) == (void *)-1)
         return -1;
-    printf("after heap: %p\n", mem_heap_hi());
 
     PUT(heap_listp, 0);                                         // 패딩
     PUT(heap_listp + (1 * WORD_SIZE), PATCH(2 * WORD_SIZE, 1)); // 프롤로그 > 헤더
@@ -81,8 +79,8 @@ int mm_init(void)
 
     heap_listp += (2 * WORD_SIZE); // 푸터의 시작주소로 이동
 
-    if (heap_listp = extend_heap(CHUNKSIZE / WORD_SIZE) == NULL) // 4096
-        return -1;
+    // if (heap_listp = extend_heap(CHUNKSIZE / WORD_SIZE) == NULL) // 4096
+    //     return -1;
 
     return 0;
 }
@@ -92,7 +90,9 @@ int mm_init(void)
  */
 void *extend_heap(size_t words)
 {
-    size_t asize = WORD_SIZE * ALIGN(words);
+    // size_t asize = WORD_SIZE * ALIGN(words); // mm_init에서 초기화했을때
+    size_t asize = ALIGN(words + SIZE_T_SIZE);
+
     void *bp;
     if ((bp = mem_sbrk(asize)) == (void *)-1)
         return NULL;
@@ -119,8 +119,30 @@ void *coalesing(void *bp)
     // case1 앞 뒤 모두 할당중일때
     if (GET_ALLOC(HDRP(prev_block)) && GET_ALLOC(HDRP(next_block)))
         return bp;
-    // case2 앞만 할당중 (뒤가 free)
-    else if (GET_ALLOC(prev_block))
+    // case2 앞 뒤 free
+    else if (!GET_ALLOC(HDRP(prev_block)) && !GET_ALLOC(HDRP(next_block)))
+    {
+        size_t aszie = GET_SIZE(HDRP(prev_block)) + GET_SIZE(HDRP(bp)) + GET_SIZE(HDRP(next_block));
+
+        PUT(HDRP(prev_block), PATCH(aszie, 0));
+        PUT(FTRP(next_block), PATCH(aszie, 0));
+
+        return prev_block;
+    }
+    // case3 뒤만 할당중 (앞이 free)
+    else if (GET_ALLOC(HDRP(next_block)))
+    {
+        // 현재 블록과 앞 블록의 사이즈를 합친다.
+        // 앞의 블록의 헤더와 푸터를 변형한다.
+        size_t aszie = GET_SIZE(HDRP(prev_block)) + GET_SIZE(HDRP(bp));
+
+        PUT(HDRP(prev_block), PATCH(aszie, 0));
+        PUT(FTRP(bp), PATCH(aszie, 0));
+
+        return prev_block;
+    }
+    // case4  앞만 할당중 (뒤가 free)
+    else
     {
         // 현재 블록과 뒷 블록의 사이즈를 합친다.
         // 앞의 블록의 헤더와 푸터를 변형한다.
@@ -131,28 +153,6 @@ void *coalesing(void *bp)
 
         return bp;
     }
-    // case3 뒤만 할당중 (앞이 free)
-    else if (GET_ALLOC(next_block))
-    {
-        // 현재 블록과 앞 블록의 사이즈를 합친다.
-        // 앞의 블록의 헤더와 푸터를 변형한다.
-        size_t aszie = GET_SIZE(FTRP(prev_block)) + GET_SIZE(HDRP(bp));
-
-        PUT(HDRP(prev_block), PATCH(aszie, 0));
-        PUT(FTRP(bp), PATCH(aszie, 0));
-
-        return prev_block;
-    }
-    // case4 앞 뒤 free
-    else
-    {
-        size_t aszie = GET_SIZE(FTRP(prev_block)) + GET_SIZE(HDRP(bp)) + GET_SIZE(HDRP(next_block));
-
-        PUT(HDRP(prev_block), PATCH(aszie, 0));
-        PUT(FTRP(next_block), PATCH(aszie, 0));
-
-        return prev_block;
-    }
 
     return NULL;
 }
@@ -160,18 +160,93 @@ void *coalesing(void *bp)
 /*
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
+ *     brk 포인터를 증가시켜 블록을 할당합니다.
+ *     항상 정렬 값의 배수 크기의 블록을 할당하십시오.
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
+    if (size < 1)
         return NULL;
-    else
+
+    void *p;
+    if ((p = find_fit(size)) == NULL)
+        return NULL;
+
+    return p;
+}
+
+/*
+ * find_fit - first fit
+ * 적합한 블록을 처음 찾으면 바로 넣는다.
+ */
+void *find_fit(size_t size)
+{
+    if (size < 1)
+        return NULL;
+
+    void *cur_bp = heap_listp;
+    size_t asize = ALIGN(size + SIZE_T_SIZE);
+
+    while (GET_SIZE(HDRP(cur_bp)) > 0) // cur_bp가 NULL이 아니거나 free block 중에 size가 적합한 블록이 있을 경우
     {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+        if (GET_SIZE(HDRP(cur_bp)) >= asize && GET_ALLOC(HDRP(cur_bp)) == 0)
+        {
+            if ((cur_bp = place(cur_bp, asize)) == (void *)-1)
+                return NULL;
+            return cur_bp;
+        }
+        cur_bp = NEXT_BLOCK_PTR(cur_bp);
     }
+
+    if ((cur_bp = extend_heap(asize)) == (void *)-1)
+        return NULL;
+
+    if ((cur_bp = place(cur_bp, asize)) == (void *)-1)
+        return NULL;
+
+    return cur_bp;
+}
+
+void *place(void *bp, size_t asize)
+{
+    if (bp == NULL || asize < 1)
+        return (void *)-1;
+
+    size_t minimum_block_size = 2 * DOUBLE_WORD_SIZE;
+    size_t cur_block_size = GET_SIZE(HDRP(bp));
+
+    if ((cur_block_size - asize) >= minimum_block_size)
+    {
+        void *temp = splitting(bp, asize);
+        if (temp != NULL)
+            bp = temp;
+    }
+
+    PUT(HDRP(bp), PATCH(GET_SIZE(HDRP(bp)), 1));
+    PUT(FTRP(bp), PATCH(GET_SIZE(HDRP(bp)), 1));
+
+    return bp;
+}
+
+void *splitting(void *bp, size_t size)
+{
+    if (bp == NULL || size < 1)
+        return NULL;
+
+    size_t origin_size = GET_SIZE(HDRP(bp));
+
+    // bp의 헤더를 size만큼으로 변경
+    // bp의 푸터를 size만큼으로 변경
+    PUT(HDRP(bp), PATCH(size, 0));
+    PUT(FTRP(bp), PATCH(size, 0));
+
+    // 그다음 헤더를 남은 사이즈 만큼 free 블록으로 변경
+    // 그다음 푸터를 남은 사이즈 만큼 free 블록으로 변경
+    void *next_block = NEXT_BLOCK_PTR(bp);
+    PUT(HDRP(next_block), PATCH(origin_size - size, 0));
+    PUT(FTRP(next_block), PATCH(origin_size - size, 0));
+
+    return bp;
 }
 
 /*
--- week07/malloc_lab_docker-master/malloc-lab/mm.h
+++ week07/malloc_lab_docker-master/malloc-lab/mm.h
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
-extern int mm_init (void);
-extern void *mm_malloc (size_t size);
-extern void mm_free (void *ptr);
+extern int mm_init(void);
+extern void *mm_malloc(size_t size);
+extern void mm_free(void *ptr);
 extern void *mm_realloc(void *ptr, size_t size);
+extern void *extend_heap(size_t words);
+extern void *coalesing(void *bp);
+extern void *find_fit(size_t size);
+extern void *place(void *bp, size_t asize);
+extern void *splitting(void *bp, size_t size);
 
-
-/* 
- * Students work in teams of one or two.  Teams enter their team name, 
+/*
+ * Students work in teams of one or two.  Teams enter their team name,
  * personal names and login IDs in a struct of this
  * type in their bits.c file.
  */
-typedef struct {
+typedef struct
+{
     char *teamname; /* ID1+ID2 or ID1 */
     char *name1;    /* full name of first member */
     char *id1;      /* login ID of first member */
@@ -20,4 +25,3 @@ typedef struct {
 } team_t;
 
 extern team_t team;
-
