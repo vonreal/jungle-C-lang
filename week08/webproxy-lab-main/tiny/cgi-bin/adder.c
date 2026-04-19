93a632018f29e51357ece031891256c82b1e7c10
jiun <vonreal20@gmail.com>
2026-04-19 23:00:00 +0900

    feat(adder): adder.c

--- week08/webproxy-lab-main/tiny/cgi-bin/adder.c
+++ week08/webproxy-lab-main/tiny/cgi-bin/adder.c
@@ -4,7 +4,36 @@
 /* $begin adder */
 #include "csapp.h"
 
-int main(void) {
+int main(void)
+{
+  char *buf, *p;
+  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+  int n1 = 0, n2 = 0;
+
+  /* 환경변수에서 QUERY_STRING 읽기 */
+  if ((buf = getenv("QUERY_STRING")) != NULL)
+  {
+    p = strchr(buf, '&');
+    *p = '\0';
+    strcpy(arg1, buf);
+    strcpy(arg2, p + 1);
+    n1 = atoi(arg1);
+    n2 = atoi(arg2);
+  }
+
+  /* HTML 응답 body 구성 */
+  sprintf(content, "Welcome to add.com: ");
+  sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
+  sprintf(content, "%sThanks for visiting!\r\n", content);
+
+  /* HTTP 응답 헤더 출력 (stdout → 클라이언트로 리다이렉트됨) */
+  printf("Connection: close\r\n");
+  printf("Content-length: %d\r\n", (int)strlen(content));
+  printf("Content-type: text/html\r\n\r\n");
+  printf("%s", content);
+  fflush(stdout);
   exit(0);
 }
+
 /* $end adder */
